b0af7c5b38043e0c5c687d17dccba4b0af9cda03
soy juan <cotestar1@gmail.com>
2026-06-09 23:57:07 -0500

    fix Vendedor

--- mi_api/Vendedor/repository/product_repository.py
+++ mi_api/Vendedor/repository/product_repository.py
@@ -1,30 +1,35 @@
 from domain.product_domain import Product
-from typing import List, Optional
+from typing import Dict, List, Optional
+
 
 class ProductRepository:
     def __init__(self):
-        # In-memory database: {product_id: Product}
-        self._db: dict[str, Product] = {}
+        # {merchant_id: {product_id: Product}}
+        self._db: Dict[str, Dict[str, Product]] = {}
 
-    def save(self, product: Product) -> Product:
-        self._db[product.product_id] = product
+    def save(self, merchant_id: str, product: Product) -> Product:
+        if merchant_id not in self._db:
+            self._db[merchant_id] = {}
+        self._db[merchant_id][product.product_id] = product
         return product
 
-    def get_by_id(self, product_id: str) -> Optional[Product]:
-        return self._db.get(product_id)
+    def get_by_id(self, merchant_id: str, product_id: str) -> Optional[Product]:
+        return self._db.get(merchant_id, {}).get(product_id)
 
-    def get_all(self) -> List[Product]:
-        return list(self._db.values())
+    def get_all(self, merchant_id: str) -> List[Product]:
+        return list(self._db.get(merchant_id, {}).values())
 
-    def update(self, product_id: str, updated_product: Product) -> Optional[Product]:
-        if product_id in self._db:
+    def update(self, merchant_id: str, product_id: str, updated_product: Product) -> Optional[Product]:
+        merchant_store = self._db.get(merchant_id, {})
+        if product_id in merchant_store:
             updated_product.product_id = product_id
-            self._db[product_id] = updated_product
+            merchant_store[product_id] = updated_product
             return updated_product
         return None
 
-    def delete(self, product_id: str) -> bool:
-        if product_id in self._db:
-            del self._db[product_id]
+    def delete(self, merchant_id: str, product_id: str) -> bool:
+        merchant_store = self._db.get(merchant_id, {})
+        if product_id in merchant_store:
+            del merchant_store[product_id]
             return True
         return False
